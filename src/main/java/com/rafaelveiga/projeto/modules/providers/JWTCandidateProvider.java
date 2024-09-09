0a28eb8714ddffce571a44cb712044cc2957ebc0
Rafael Veiga <rafaelveig@hotmail.com>
2024-09-09 10:44:00 -0300

    🚀 feat(JWTCandidateProvider): add JWT Candidate Provider for token validation 🚀 feat(SecurityCandidateFilter): add filter for candidate authentication 🚀 feat(SecurityConfig): add SecurityCandidateFilter to security filter chain 🚀 feat(SecurityFilter): update filter logic for company authentication

---
+++ src/main/java/com/rafaelveiga/projeto/modules/providers/JWTCandidateProvider.java
@@ -0,0 +1,31 @@
+package com.rafaelveiga.projeto.modules.providers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+@Service
+public class JWTCandidateProvider {
+
+  @Value("${security.token.secret.candidate}")
+  private String secret;
+
+  public DecodedJWT validateToken(String token) {
+    token = token.replace("Bearer ", "");
+
+    Algorithm algorithm = Algorithm.HMAC256(secret);
+
+    try {
+      var tokenDecoded = JWT.require(algorithm).build().verify(token);
+
+      return tokenDecoded;
+    } catch (JWTVerificationException e) {
+      e.printStackTrace();
+      throw new JWTVerificationException("Token inválido");
+    }
+  }
+}
--- src/main/java/com/rafaelveiga/projeto/security/SecurityCandidateFilter.java
+++ src/main/java/com/rafaelveiga/projeto/security/SecurityCandidateFilter.java
@@ -1,13 +1,11 @@
 package com.rafaelveiga.projeto.security;
 
-import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.rafaelveiga.projeto.modules.providers.JWTProvider;
+import com.rafaelveiga.projeto.modules.providers.JWTCandidateProvider;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -17,32 +15,31 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class SecurityFilter extends OncePerRequestFilter {
+public class SecurityCandidateFilter extends OncePerRequestFilter {
 
   @Autowired
-  private JWTProvider jwtProvider;
+  private JWTCandidateProvider jwtCandidateProvider;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
 
-    if (request.getRequestURI().contains("/companies")) {
+    if (request.getRequestURI().contains("/candidates")) {
+
       SecurityContextHolder.getContext().setAuthentication(null);
       String header = request.getHeader("Authorization");
 
       if (header != null) {
-        var subjectToken = this.jwtProvider.validateToken(header);
-        if (subjectToken.isEmpty()) {
+        var token = this.jwtCandidateProvider.validateToken(header);
+
+        if (token == null) {
           response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
           return;
         }
 
-        request.setAttribute("company_id", subjectToken);
+        request.setAttribute("candidate_id", token.getSubject());
 
-        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null,
-            Collections.emptyList());
-
-        SecurityContextHolder.getContext().setAuthentication(auth);
+        System.out.println("Candidate ID: " + token.getSubject());
       }
 
     }
